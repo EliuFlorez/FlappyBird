b9d4023bac3ca0dd8011032c7d53408892d811ae
Eliu Florez <eliufz@gmail.com>
2018-02-10 17:57:28 -0400

    Proyect MonoGame

---
+++ FlappyBird/Helpers/Debug.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Microsoft.Xna.Framework;
+
+namespace FlappyBird.Helpers
+{
+    class Debug
+    {
+        public static void DrawHitboxes(List<Rectangle> rectangles)
+        {
+            Statics.GAME_SPRITEBATCH.Begin();
+
+            foreach (Rectangle rect in rectangles)
+            {
+                Statics.GAME_SPRITEBATCH.Draw(Statics.TEXTURE_PIXEL, rect, Statics.COLOR_HITBOX);
+            }
+
+            Statics.GAME_SPRITEBATCH.End();
+        }
+
+        public static void DrawText(Vector2 position, String text)
+        {
+            Statics.GAME_SPRITEBATCH.Begin();
+            Statics.GAME_SPRITEBATCH.DrawString(Statics.MANAGER_FONT.Library["Small"], text, position, Color.White);
+            Statics.GAME_SPRITEBATCH.End();
+        }
+    }
+}
